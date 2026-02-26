3cb707e4d2548b716be20f9f01caeab36ae909f0
arla1020 <arla1020@storsjogymnasiet.se>
2026-02-26 15:54:49 +0100

    wgwbtr

--- NiceGui/todo.py
+++ NiceGui/todo.py
@@ -1,11 +1,21 @@
 from nicegui import ui
 
 todos = []
+import ast
+
+#ladda tidigare data
+try:
+    with open("todo_data.txt", "r", encoding="utf-8") as f:
+        data = ast.literal_eval(f.read())
+        todos = data["todos"]
+except:
+    pass
+
 
 dark = ui.dark_mode()
 
 switch = ui.switch('smek mej')
-dark.bind_value(switch)  # <-- DETTA är det rätta
+dark.bind_value(switch) 
 
 ui.label("ToDo List📝").classes("text-3xl")
 
@@ -21,7 +31,17 @@ def add_task():
     for task in todos:
         with list_container:
             ui.label(task).classes("border p-2 rounded w-80 bg-purple-200")
+    
+    with open("todo_data.txt", "w", encoding="utf-8") as f:
+        f.write(str({"todos": todos, "todos": todos}))
+
+for task in todos:
+        with list_container:
+            ui.label(task).classes("border p-2 rounded w-80 bg-purple-200")
 
 ui.button("Lägg till", on_click=add_task)
+
+
+
 
 ui.run(native=True)
